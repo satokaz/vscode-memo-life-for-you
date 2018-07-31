874ff2a20295e3a3cf465e2de04cc8f6d2bb1390
satokaz <kazus.sunfish@gmail.com>
2018-07-31 14:15:21 +0900

    :lipstick:

--- src/memoOpenChrome.ts
+++ src/memoOpenChrome.ts
@@ -16,16 +16,31 @@ export class memoOpenChrome extends memoConfigure {
     /**
      * Open Chrome
      */
-    public OpenChrome() {
+    public async OpenChrome() {
         this.readConfig();
+
+        // await vscode.commands.executeCommand('markdown.showPreviewToSide').then(() => {
+        // // await vscode.commands.executeCommand('markdown-preview-enhanced.openPreview').then(() => {
+        //     setTimeout(() => { Ω
+        //         vscode.commands.executeCommand('editor.action.webvieweditor.selectAll').then(() => {
+        //             // setTimeout(() => { 
+        //                 vscode.commands.executeCommand('copy');
+
+        //             // }, 1000);
+        //         });
+        //     }, 5000);
+        //     // await vscode.commands.executeCommand('workbench.action.');
+        // });
+
         chromeLauncher.launch({
             // startingUrl: 'data:text/html, <html contenteditable>',
+            // startingUrl: data:text/html, <html contenteditable><style>body{background-color:#272822;color:#ddd;margin:50px 100px}</style>
             startingUrl: this.memoOpenChromeCustomizeURL,
             chromeFlags: ['--no-default-browser-check']
             // https://peter.sh/experiments/chromium-command-line-switches/
             // startingUrl: 'data:text/html, <html contenteditable><style type="text/css">*{caret-color: blue;}</style>',
         }).then(chrome => {
-            console.log(`Chrome debugging port running on ${chrome.port}`);
+            // console.log(`Chrome debugging port running on ${chrome.port}`);
         });
     }
 }
